fadbb4d3069f553336ff280ebd12feb9e3a6a50c
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-01 19:37:22 +1000

    Update main.tf

--- examples/terraform/main.tf
+++ examples/terraform/main.tf
@@ -1,1 +1,19 @@
+# Minimal Terraform example (educational)
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-demo-rg"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-demo-vnet"
+  address_space       = ["10.2.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Note: add VM and other resources as exercises
 
