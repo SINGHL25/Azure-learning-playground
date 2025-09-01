21217768d8f8b4ea4b53a7173f2d315e1a9054e6
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-01 19:41:37 +1000

    Update test_vnet_manager.py

--- tests/test_vnet_manager.py
+++ tests/test_vnet_manager.py
@@ -1,1 +1,8 @@
 
+from src.vnet_manager import create_vnet_demo, create_peering_demo
+
+def test_vnet_and_peering():
+    v = create_vnet_demo("testvnet")
+    p = create_peering_demo("v1","v2")
+    assert "vnet" in v
+    assert p["status"] == "peered"
