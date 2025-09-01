2a8d69f0009af6bbacc16c43977d2fc885b6e798
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-01 19:38:54 +1000

    Update create_vm.sh

--- examples/scripts/create_vm.sh
+++ examples/scripts/create_vm.sh
@@ -1,1 +1,7 @@
 
+#!/bin/bash
+# Minimal script to create a VM (requires az cli and login)
+RESOURCE_GROUP="demo-rg"
+VM_NAME="demo-vm"
+az group create -n $RESOURCE_GROUP -l eastus
+az vm create -g $RESOURCE_GROUP -n $VM_NAME --image UbuntuLTS --admin-username azureuser --generate-ssh-keys
