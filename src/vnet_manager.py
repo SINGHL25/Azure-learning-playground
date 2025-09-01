f8ddb7a0f5005dd4b4ff17a82556881b3f6d7bdb
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-01 19:36:05 +1000

    Update vnet_manager.py

--- src/vnet_manager.py
+++ src/vnet_manager.py
@@ -1,1 +1,7 @@
 
+def create_vnet_demo(name="demoVNet", cidr="10.1.0.0/16"):
+    # Simulated VNet object
+    return {"vnet":name,"cidr":cidr,"subnets":[{"name":"default","cidr":"10.1.0.0/24"}]}
+
+def create_peering_demo(vnet1, vnet2, allow_forwarding=False):
+    return {"from":vnet1,"to":vnet2,"status":"peered","allow_forwarding":allow_forwarding}
